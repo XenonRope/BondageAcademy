1bb5fc47c0b67a2a875145879538c2f984454fc1
XenonRope <xenonrope@example.com>
2023-11-11 17:26:24 +0100

    Improve camera in world view

--- client/src/world/WorldView.tsx
+++ client/src/world/WorldView.tsx
@@ -1,26 +1,71 @@
-import { For } from "solid-js";
+import { trackBounds } from "solid-boundaries";
+import { For, createMemo } from "solid-js";
 import { socketService } from "../common/SocketService";
-import type { Position } from "../common/model/Position";
+import { storeService } from "../store/StoreService";
+import { isPlayerObject, type PlayerObject } from "./model/PlayerObject";
 import { WORLD_TILE_SIZE, type World } from "./model/World";
 import ObjectView from "./object/ObjectVIew";
 
 export default function WorldView(props: { world: World }) {
+  const { ref, bounds } = trackBounds();
+
   async function move(event: MouseEvent & { currentTarget: HTMLDivElement }) {
     const rect = event.currentTarget?.getBoundingClientRect();
-    const targetPosition: Position = {
-      x: Math.floor((event.clientX - rect.left) / 48),
-      y: Math.floor((event.clientY - rect.top) / 48),
-    };
-    await socketService.emit("set_player_target_position", targetPosition);
+    const x = Math.floor((event.clientX - rect.left - offset().x) / 48);
+    const y = Math.floor((event.clientY - rect.top - offset().y) / 48);
+    if (x >= 0 && x < props.world.width && y >= 0 && y < props.world.height) {
+      await socketService.emit("set_player_target_position", { x, y });
+    }
   }
 
+  const offset = createMemo(() => {
+    const playerId = storeService.getStore().player?.id;
+    const player = Object.values(props.world.objects).find(
+      (object): object is PlayerObject =>
+        isPlayerObject(object) && object.playerId === playerId,
+    );
+
+    const halfScreenWidth = (bounds()?.width ?? 0) / 2;
+    const halfScreenHeight = (bounds()?.height ?? 0) / 2;
+    const worldWidth = props.world.width * WORLD_TILE_SIZE;
+    const worldHeight = props.world.height * WORLD_TILE_SIZE;
+    let x =
+      -(player?.motion?.currentPosition.x ?? player?.position.x ?? 0) *
+        WORLD_TILE_SIZE +
+      halfScreenWidth -
+      WORLD_TILE_SIZE / 2;
+    let y =
+      -(player?.motion?.currentPosition.y ?? player?.position.y ?? 0) *
+        WORLD_TILE_SIZE +
+      halfScreenHeight -
+      WORLD_TILE_SIZE / 2;
+
+    if (x > 0) {
+      x = 0;
+    } else if (x < -worldWidth + (bounds()?.width ?? 0)) {
+      x = -worldWidth + (bounds()?.width ?? 0);
+    }
+    if (y > 0) {
+      y = 0;
+    } else if (y < -worldHeight + (bounds()?.height ?? 0)) {
+      y = -worldHeight + (bounds()?.height ?? 0);
+    }
+
+    return { x, y };
+  });
+
   return (
-    <div onClick={move} class="w-full h-full overflow-hidden">
+    <div
+      ref={ref}
+      onClick={move}
+      class="w-full h-full overflow-hidden bg-green-100"
+    >
       <div
         class="relative"
         style={{
           width: `${props.world.width * WORLD_TILE_SIZE}px`,
           height: `${props.world.height * WORLD_TILE_SIZE}px`,
+          transform: `translate(${offset().x}px, ${offset().y}px)`,
         }}
       >
         <For each={Object.values(props.world.objects)}>
