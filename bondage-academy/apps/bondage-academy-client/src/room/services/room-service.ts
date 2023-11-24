c70857efe871bec990c9d415e99c1c503ef821dd
XenonRope <xenonrope@example.com>
2023-11-24 15:34:01 +0100

    Unselect player after leaving room

--- bondage-academy/apps/bondage-academy-client/src/room/services/room-service.ts
+++ bondage-academy/apps/bondage-academy-client/src/room/services/room-service.ts
@@ -54,6 +54,7 @@ export class RoomService {
     await this.socketService.emit(RequestFromClient.LeaveRoom, undefined);
     this.storeService.setRoom(undefined);
     this.storeService.clearChatMessages();
+    this.storeService.selectPlayer(undefined);
   }
 
   async searchRooms(request: SearchRoomRequest): Promise<SearchRoomResponse> {
