3a2ccd9c811d649d26b5dda79bce0a4b9f7265a7
XenonRope <xenonrope@example.com>
2023-12-23 16:19:20 +0100

    Change dialogue button style

--- bondage-academy/apps/bondage-academy-client/src/chat/dialogue-option-view.tsx
+++ bondage-academy/apps/bondage-academy-client/src/chat/dialogue-option-view.tsx
@@ -1,16 +1,13 @@
 import { JSX } from "solid-js/jsx-runtime";
+import Button from "../ui/button";
 
 export default function DialogueOptionView(props: {
   children?: JSX.Element;
   onClick?: () => void;
 }) {
   return (
-    <button
-      type="button"
-      class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-2 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-2 py-1 text-center"
-      onClick={() => props.onClick?.()}
-    >
+    <Button onClick={() => props.onClick?.()} size="small">
       {props.children}
-    </button>
+    </Button>
   );
 }
