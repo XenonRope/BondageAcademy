750331ee21338998ef94dea879108811d284a5c1
XenonRope <xenonrope@example.com>
2024-02-21 18:32:28 +0100

    Wait for lock during provisioning

--- devops/prod/init-script.sh
+++ devops/prod/init-script.sh
@@ -3,8 +3,8 @@
 sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" "/etc/needrestart/needrestart.conf"
 
 # Install docker
-sudo apt-get -y update
-sudo apt-get -y install ca-certificates curl
+sudo apt-get -y -o DPkg::Lock::Timeout=60 update
+sudo apt-get -y -o DPkg::Lock::Timeout=60 install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,5 +12,5 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y -o DPkg::Lock::Timeout=60 update
+sudo apt-get -y -o DPkg::Lock::Timeout=60 install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
--- devops/prod/main.tf
+++ devops/prod/main.tf
@@ -22,24 +22,82 @@ data "digitalocean_ssh_key" "OpenTofu" {
 }
 
 resource "digitalocean_droplet" "bondage-academy-dev-1" {
-  image = "ubuntu-22-04-x64"
-  name = "bondage-academy-dev-1"
-  region = "fra1"
-  size = "s-1vcpu-2gb"
+  image      = "ubuntu-22-04-x64"
+  name       = "bondage-academy-dev-1"
+  region     = "fra1"
+  size       = "s-1vcpu-512mb-10gb"
   monitoring = true
-  ssh_keys = [
+  ssh_keys   = [
     data.digitalocean_ssh_key.OpenTofu.id
   ]
 
   connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
-    agent = true
+    host    = self.ipv4_address
+    user    = "root"
+    type    = "ssh"
+    agent   = true
     timeout = "2m"
   }
   
   provisioner "remote-exec" {
     script = "./init-script.sh"
+  }
+}
+
+resource "digitalocean_domain" "bondage-academy-dev-1-domain" {
+  name = "bondage-academy.com"
+}
+
+resource "digitalocean_record" "bondage-academy-dev-1-domain-record" {
+  domain = digitalocean_domain.bondage-academy-dev-1-domain.id
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_droplet.bondage-academy-dev-1.ipv4_address
+  ttl    = 300
+}
+
+resource "digitalocean_firewall" "bondage-academy-firewall" {
+  name = "bondage-academy-firewall"
+
+  droplet_ids = [digitalocean_droplet.bondage-academy-dev-1.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
