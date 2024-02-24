1d789a1c5380a0b1f75c7363010536f78d279432
XenonRope <xenonrope@example.com>
2024-02-24 22:44:00 +0100

    Add jest to bondage-academy-client

---
+++ bondage-academy/apps/bondage-academy-client/jest.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default {
+  displayName: "bondage-academy-client",
+  preset: "../../jest.preset.js",
+  coverageDirectory: "../../coverage/apps/bondage-academy-client",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
