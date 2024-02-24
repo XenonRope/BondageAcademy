08d66297e88ab8c57abb596d0bff808074ba62c0
XenonRope <xenonrope@example.com>
2024-02-24 13:47:42 +0100

    Enable swap

--- devops/prod/init-script.sh
+++ devops/prod/init-script.sh
@@ -14,3 +14,14 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y -o DPkg::Lock::Timeout=60 update
 sudo apt-get -y -o DPkg::Lock::Timeout=60 install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable swap
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Set swappiness to 10
+sudo sysctl vm.swappiness=10
+echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
